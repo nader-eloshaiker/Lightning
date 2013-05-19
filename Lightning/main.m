880f15b29e337e6ae6cd6c9ad92d62677fa63267
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2013-05-19 21:42:34 +1000

    Initial Commit

---
+++ Lightning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lightning
+//
+//  Created by Nader Eloshaiker on 19/05/13.
+//  Copyright (c) 2013 Nader Eloshaiker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
